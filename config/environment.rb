9e18e788ed047d2f762384fdf3b358cf928a5341
Kieran Coughlan <kcoughlan78@me.com>
2013-06-12 19:51:25 +0100

    moved host def from environment to production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,4 @@ Gameproject::Application.initialize!
 
 Time::DATE_FORMATS[:short] = "Created on %d %b at %H:%M %p"
 
-config.action_mailer.default_url_options = { :host => 'http://infinite-spire-6748.herokuapp.com' }
+
