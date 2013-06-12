5df7975b0789ec9b74aae4049ad93542274cc9d2
Kieran Coughlan <kcoughlan78@me.com>
2013-06-12 20:45:24 +0100

    added mailer details to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,18 @@ Gameproject::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      user_name: "hurlersvsmonsters",
+      password: "hurlers2013",
+      enable_starttls_auto: true
+  }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
