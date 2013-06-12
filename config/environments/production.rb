58351cee1a3ea45fb5bcfa193ee1ba2ef9717e4d
Kieran Coughlan <kcoughlan78@me.com>
2013-06-12 18:57:56 +0100

    changes made for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Gameproject::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #changed to true for initial production launch
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
