afa0252c21103efd51df1afcab47fd8757b8e10c
Kieran Coughlan <kcoughlan78@me.com>
2013-06-13 11:31:24 +0100

    moved def back to env.rb and defined delivery method in env.rb too

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,13 @@ Gameproject::Application.initialize!
 
 Time::DATE_FORMATS[:short] = "Created on %d %b at %H:%M %p"
 
-
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :authentication       => :plain,
+    :user_name            => ENV['app16268747@heroku.com'],
+    :password             => ENV['nzypgvmp'],
+    :domain               => 'heroku.com',
+    :enable_starttls_auto => true,
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,17 +62,8 @@ Gameproject::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
-      :authentication       => :plain,
-      :user_name            => ENV['app16268747@heroku.com'],
-      :password             => ENV['nzypgvmp'],
-      :domain               => 'heroku.com',
-      :enable_starttls_auto => true,
-  }
 
   #config.action_mailer.smtp_settings = {
     #  address: "smtp.gmail.com",
