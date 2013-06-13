991552e9636162747db02f0e915235b61d864a42
Kieran Coughlan <kcoughlan78@me.com>
2013-06-13 11:49:58 +0100

    changed prod and env again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,28 @@ Gameproject::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
+ # ActionMailer::Base.delivery_method = :smtp
+  #ActionMailer::Base.smtp_settings = {
+   #   :address              => 'smtp.sendgrid.net',
+   #   :port                 => '587',
+   #   :authentication       => :plain,
+   #   :user_name            => ENV['app16268747@heroku.com'],
+   #   :password             => ENV['nzypgvmp'],
+   #   :domain               => 'infinite-spire-6748.herokuapp.com',
+   #   :enable_starttls_auto => true,
+  #}
 
-  #config.action_mailer.smtp_settings = {
-    #  address: "smtp.gmail.com",
-    #  port: 587,
-    #  domain: "gmail.com",
-   #   authentication: "plain",
-   #   user_name: "hurlersvsmonsters",
-   #   password: "hurlers2013",
-   #   enable_starttls_auto: true
- # }
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :authentication       => :plain,
+      :user_name            => ENV['app16268747@heroku.com'],
+      :password             => ENV['nzypgvmp'],
+      :domain               => 'infinite-spire-6748.herokuapp.com',
+      :enable_starttls_auto => true,
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
